03558c7e6b6759e544240dc20ece628fc167e965
schoolwareapp <schoolwareapp@gmail.com>
2023-04-01 14:01:53 +0430

    Adding the fruit entity

---
+++ src/Entity/Fruit.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FruitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FruitRepository::class)]
+class Fruit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $family = null;
+
+    #[ORM\Column]
+    private ?float $carbohydrates = null;
+
+    #[ORM\Column]
+    private ?float $protein = null;
+
+    #[ORM\Column]
+    private ?float $fat = null;
+
+    #[ORM\Column]
+    private ?float $calories = null;
+
+    #[ORM\Column]
+    private ?float $sugar = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $favorite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFamily(): ?string
+    {
+        return $this->family;
+    }
+
+    public function setFamily(string $family): self
+    {
+        $this->family = $family;
+
+        return $this;
+    }
+
+    public function getCarbohydrates(): ?float
+    {
+        return $this->carbohydrates;
+    }
+
+    public function setCarbohydrates(float $carbohydrates): self
+    {
+        $this->carbohydrates = $carbohydrates;
+
+        return $this;
+    }
+
+    public function getProtein(): ?float
+    {
+        return $this->protein;
+    }
+
+    public function setProtein(float $protein): self
+    {
+        $this->protein = $protein;
+
+        return $this;
+    }
+
+    public function getFat(): ?float
+    {
+        return $this->fat;
+    }
+
+    public function setFat(float $fat): self
+    {
+        $this->fat = $fat;
+
+        return $this;
+    }
+
+    public function getCalories(): ?float
+    {
+        return $this->calories;
+    }
+
+    public function setCalories(float $calories): self
+    {
+        $this->calories = $calories;
+
+        return $this;
+    }
+
+    public function getSugar(): ?float
+    {
+        return $this->sugar;
+    }
+
+    public function setSugar(float $sugar): self
+    {
+        $this->sugar = $sugar;
+
+        return $this;
+    }
+
+    public function isFavorite(): ?bool
+    {
+        return $this->favorite;
+    }
+
+    public function setFavorite(?bool $favorite): self
+    {
+        $this->favorite = $favorite;
+
+        return $this;
+    }
+}
