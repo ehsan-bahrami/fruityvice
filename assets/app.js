3a0ff4de3dd0cff6ab4f15cf58f921fa5ec9701f
schoolwareapp <schoolwareapp@gmail.com>
2023-04-03 15:38:10 +0430

    Installing UX Vue.js

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,20 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import { registerVueControllerComponents } from '@symfony/ux-vue';
+
+// Registers Vue.js controller components to allow loading them from Twig
+//
+// Vue.js controller components are components that are meant to be rendered
+// from Twig. These component can then rely on other components that won't be
+// called directly from Twig.
+//
+// By putting only controller components in `vue/controllers`, you ensure that
+// internal components won't be automatically included in your JS built file if
+// they are not necessary.
+registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
+
+// If you prefer to lazy-load your Vue.js controller components, in order to reduce to keep the JavaScript bundle the smallest as possible,
+// and improve performances, you can use the following line instead:
+//registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/, 'lazy'));
