4eb76340ef0b458bba221e64423bcb602f13c636
schoolwareapp <schoolwareapp@gmail.com>
2023-04-05 11:03:46 +0430

    Resetting the database automatically in testing

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Vue\VueBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
