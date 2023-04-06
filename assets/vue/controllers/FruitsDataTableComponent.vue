99423d616f71648134b4b307a9204a44d55a1c50
schoolwareapp <schoolwareapp@gmail.com>
2023-04-06 17:18:40 +0430

    Improving Fruits data table styling

--- assets/vue/controllers/FruitsDataTableComponent.vue
+++ assets/vue/controllers/FruitsDataTableComponent.vue
@@ -63,9 +63,11 @@
                 <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                     style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
             </div>
-            <div class="mx-auto max-w-4xl py-32 sm:py-48 lg:py-56">
+            <div class="mx-auto max-w-4xl py-8 sm:py-16 lg:py-24">
+                <h2 class="text-2xl">Fruits</h2>
+                <hr class="mb-5">
                 <div id="error-box" class="w-full hidden">
-                    <div class="relative block w-full rounded-lg bg-indigo-300 p-4 text-base leading-5 text-white opacity-100"
+                    <div class="relative block w-full rounded-lg bg-indigo-300 p-4 text-base leading-5 text-white opacity-100 mb-5"
                         data-dismissible="alert">
                         <div class="mr-12">An error has occurred! You must have a maximum of 10 favorite fruits.</div>
                     </div>
